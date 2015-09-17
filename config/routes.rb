f1c295689dc24f358088794c911ceafa02b03b89
Ariff Azraai <ariff@azraai.me>
2015-09-17 14:04:31 +0800

    Update landing page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :project, except: :destroy do
+  	put :trash, on: :member
+  end
+  resources :users, except: :destroy
+  get 'account', to: 'account#edit',	as: :account
+  put 'account', to: 'account#update'
+  get 'submit', to: 'home#submit', as: :submit
+  post 'submit', to: 'home#submitted'
   root 'home#index'
 end
