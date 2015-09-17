3e41bb40392d9443aa3cc54de28a5d186194b0ee
Ariff Azraai <ariff@azraai.me>
2015-09-17 18:20:59 +0800

    Fix user migration.

--- db/migrate/20150916135753_devise_create_users.rb
+++ db/migrate/20150916135753_devise_create_users.rb
@@ -6,7 +6,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :encrypted_password, null: false, default: ""
 
       t.string :name
-      t.string :phone
 
       ## Recoverable
       t.string   :reset_password_token
