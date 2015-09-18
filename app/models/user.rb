a39a7ace0ab1f2e83531c9c7ffcfbee45aff4361
Ariff Azraai <ariff@azraai.me>
2015-09-18 21:27:57 +0800

    User seed data.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+  include ActiveModel::Transitions
+  
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true
+
+	state_machine do
+    state :active
+    state :suspended
+  end
 end
